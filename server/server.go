9338574bbb0a1e8ffd75518f163fd4781bc95ed2
byacine69360@gmail.com <pythag0re@ymachine>
2025-05-19 09:03:10 +0200

    landing page in server

--- server/server.go
+++ server/server.go
@@ -58,6 +58,25 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func landingHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/landing" {
+		http.NotFound(w, r)
+		fmt.Printf("Error: handler for %s not found\n", html.EscapeString(r.URL.Path))
+		return
+	}
+
+	if r.Method == http.MethodGet {
+		tmpl := template.Must(template.ParseFiles("_templates_/landing.html"))
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Printf("erreur de template %s:", err)
+		}
+	} else if r.Method == http.MethodPost {
+		controllers.RegisterUser(w, r)
+	}
+}
+
 func Start() {
 	db.InitDB()
 	defer db.CloseDB()
@@ -66,12 +85,13 @@ func Start() {
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("_templates_/"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		http.Redirect(w, r, "/landing", http.StatusSeeOther)
 	})
 
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/landing", landingHandler)
 
 	fmt.Println("Serveur démarré sur le port 8080 ")
 	http.ListenAndServe(":8080", nil)
