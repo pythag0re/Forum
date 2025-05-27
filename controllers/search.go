4ddb5ce9838c74e363166653dabcd154608895bd
Bernier-malik <malik.bernier@ynov.com>
2025-05-27 09:57:51 +0200

    connection + request a la db

--- controllers/search.go
+++ controllers/search.go
@@ -6,15 +6,16 @@ import (
     _ "github.com/mattn/go-sqlite3"
 )
 
-func getPost(title string)  {
+func getPost(title string) (*sql.Rows, error)  {
 	db, err := sql.Open("sqlite3", "../app.db")
 	if err != nil {
 		fmt.Println("la data base n'a pas fonctionné")
 	} else {
 		rows, err := db.Query("SELECT id, name FROM post WHERE title LIKE ?", "%"+title+"%")
-
+		if err != nil {
+			return nil, err
+		}
+		return rows, nil
 	}
-
-
-
+	return nil, err
 } 
